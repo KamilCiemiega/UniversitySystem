08ad3a5e200905b456a943417bd2807673c04f3b
kamilciemiega <kamil13241012@interia.pl>
2025-03-14 22:53:02 +0100

    c

--- src/main/java/com/kamilc/universitysystem/security/JwtUtil.java
+++ src/main/java/com/kamilc/universitysystem/security/JwtUtil.java
@@ -65,8 +65,8 @@ public class JwtUtil {
                 .getBody();
     }
 
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
+//    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+//        final Claims claims = extractAllClaims(token);
+//        return claimsResolver.apply(claims);
+//    }
 }
