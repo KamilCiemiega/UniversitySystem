49f2716ac3a766b780b5be5e618348685ca6177b
kamilciemiega <kamil13241012@interia.pl>
2025-04-04 20:00:49 +0200

    json

--- src/main/java/com/kamilc/universitysystem/domain/service/RecruitmentScoringService.java
+++ src/main/java/com/kamilc/universitysystem/domain/service/RecruitmentScoringService.java
@@ -2,6 +2,6 @@ package com.kamilc.universitysystem.domain.service;
 
 import com.kamilc.universitysystem.entity.Application;
 
-public interface RecruitmentScoringService {
-    Application calculateScore(Application app);
-}
+//public interface RecruitmentScoringService {
+//    Application calculateScore(Application app);
+//}
